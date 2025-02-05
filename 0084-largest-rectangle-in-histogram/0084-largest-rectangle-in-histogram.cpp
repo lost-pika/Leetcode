908a8c0f772ea0b94fa23d17dceab0f63567d321
Srishti Verma <136918651+sinceresrishti@users.noreply.github.com>
2025-02-06 02:42:55 +0530

    Time: 56 ms (12.12%), Space: 85.9 MB (34.12%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,5 +1,3 @@
-// \U0001f4c2 Problem 4: Largest Rectangle Area in Histogram (Leectcode-84)
-
 class Solution {
 public:
     vector<int> nextSmallerElement(vector<int>&arr){
@@ -10,14 +8,10 @@ public:
 
         for(int i=size-1; i>=0; i--){
             int currElement = arr[i];
-
-            // Find ans to currElement from stack
             while (st.top() != -1 && arr[st.top()] >= currElement)
             {
                 st.pop();
             }
-
-            // Ynha tak me tabhi pahuncha hoga jab st.top() < currElement
             ans[i] = st.top();
             st.push(i);        
         }
@@ -32,14 +26,10 @@ public:
 
         for(int i=0; i<size; i++){
             int currElement = arr[i];
-
-            // Find ans to currElement from stack
             while (st.top() != -1 && arr[st.top()] >= currElement)
             {
                 st.pop();
             }
-
-            // Ynha tak me tabhi pahuncha hoga jab st.top() < currElement
             ans[i] = st.top();
             st.push(i);        
         }
@@ -47,25 +37,16 @@ public:
     }
 
     int largestRectangleArea(vector<int>& heights) {
-        // Step A: Next smaller element index
         vector<int> next = nextSmallerElement(heights);
-
-        // Make sure karna ki next me koi bhi index -1 na ho
-        // yadi hai to use next ke size se update kardo
         for(int i=0; i<next.size(); i++){
             if(next[i] == -1){
                 next[i] = next.size();
             }
         }
-
-        // Step B: Prev smaller element index
         vector<int> prev = prevSmallerElement(heights);
-
-        // Now find the max area
         int maxArea = INT_MIN;
 
         for(int i=0; i<heights.size(); i++){
-            // Step C: Find the width
             int height = heights[i];
             int width = next[i] - prev[i] - 1;
             int currArea = width * height;
