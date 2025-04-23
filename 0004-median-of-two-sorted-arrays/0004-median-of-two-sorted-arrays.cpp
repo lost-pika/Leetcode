2c0d3333873aaf4063ddef389f9be729783c7d5c
Srishti Verma <136918651+lost-pika@users.noreply.github.com>
2025-04-23 16:57:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -6,24 +6,52 @@ public:
         int n2 = nums2.size();
         int i = 0;
         int j = 0;
-        while(i < n1 && j < n2){
-            if(nums1[i] < nums2[j]){
-                nums3.push_back(nums1[i++]);
-            }
-            else{
-                nums3.push_back(nums2[j++]);
-            }
-        }
-
-        while(i < n1) nums3.push_back(nums1[i++]);
-        while(j < n2) nums3.push_back(nums2[j++]);
-
         int n = (n1 + n2);
-        if(n % 2 == 1){
-            return nums3[n/2];
+        int ind2 = n / 2;
+        int ind1 = ind2 - 1;
+        int ind1el = -1;
+        int ind2el = -1;
+        int cnt = 0;
+        while (i < n1 && j < n2) {
+            if (nums1[i] < nums2[j]) {
+                if (cnt == ind1)
+                    ind1el = nums1[i];
+                if (cnt == ind2)
+                    ind2el = nums2[j];
+                cnt++;
+                i++;
+            } else {
+                if (cnt == ind1)
+                    ind1el = nums1[i];
+                if (cnt == ind2)
+                    ind2el = nums2[j];
+                cnt++;
+                j++;
+            }
         }
-        else{
-            return ((double)((double)nums3[n/2] + (double)nums3[n/2 - 1]))/2.0;
+
+        while (i < n1) {
+            if (cnt == ind1)
+                ind1el = nums1[i];
+            if (cnt == ind2)
+                ind2el = nums1[i];
+            cnt++;
+            i++;
+        }
+        while (j < n2) {
+            if (cnt == ind1)
+                ind1el = nums2[j];
+            if (cnt == ind2)
+                ind2el = nums2[j];
+            cnt++;
+            j++;
+        }
+
+        if (n % 2 == 1) {
+            return ind2el;
+        } else {
+            return ((double)((double)ind1el + (double)ind2el)) /
+                   2.0;
         }
     }
 };
