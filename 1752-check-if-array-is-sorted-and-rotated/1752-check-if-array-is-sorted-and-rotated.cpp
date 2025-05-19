b92973dec5171cec6a5ef67f490598754b5c0c23
Srishti Verma <136918651+lost-pika@users.noreply.github.com>
2025-05-19 18:36:59 +0530

    Time: 3 ms (2.72%), Space: 11.9 MB (5.89%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int count = 0;  // Count how many times the order is violated
         int n = nums.size();
+        vector<int> sorted(n);
 
-        for(int i = 0; i < n; i++) {
-            if(nums[i] > nums[(i+1) % n]) {  // Check for decreasing order
-                count++;
+        for(int r = 0; r < n; r++){
+            int index = 0;
+            for(int i = r; i<n; i++){
+                sorted[index] = nums[i];
+                index++;
+            }
+
+            for(int i = 0; i<r; i++){
+                sorted[index] = nums[i];
+                index++;
+            }
+
+            bool isSorted = true;
+
+            // check if sorted
+            for(int i = 0; i<n-1; i++){
+                if(sorted[i] > sorted[i+1]){
+                    isSorted = false;
+                    break;
+                }
+            }
+
+            if(isSorted == true){
+                return true;
             }
         }
-
-        return count <= 1; // It should have at most one drop to be a rotated sorted array
+        return false;
     }
 };
