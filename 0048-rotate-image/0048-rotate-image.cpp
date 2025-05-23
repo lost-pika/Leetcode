23a76fa7758e872bce61d6c4fc8622f835c997d6
Srishti Verma <136918651+lost-pika@users.noreply.github.com>
2025-05-23 23:18:55 +0530

    Time: 0 ms (100%), Space: 10.2 MB (34.56%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int m = matrix[0].size();
 
         // Transpose
         for(int i = 0; i<n; i++){
-            for(int j = i; j<m; j++){
+            for(int j = i; j<n; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
