54e644920d3e746123a7067344b5151035467424
Srishti Verma <136918651+lost-pika@users.noreply.github.com>
2025-05-23 18:21:46 +0530

    Time: 0 ms (100%), Space: 23.9 MB (49.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,10 +1,5 @@
 class Solution {
 public:
-    void swap(int i, int j, vector<int> &nums){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int j = -1;
@@ -21,7 +16,7 @@ public:
 
         for(int i = j+1; i<n; i++){
             if(nums[i] != 0){
-                swap(i, j, nums);
+                swap(nums[i], nums[j]);
                 j++;
             }
         }
