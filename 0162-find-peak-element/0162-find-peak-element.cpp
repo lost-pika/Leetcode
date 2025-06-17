ee423b174071960bfac46b80e19e3f3646b76429
Srishti Verma <136918651+lost-pika@users.noreply.github.com>
2025-06-17 21:51:26 +0530

    Time: 0 ms (100%), Space: 12.5 MB (66.79%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,17 +8,20 @@ public:
         int low = 1;
         int high = n-2;
         while(low <= high){
-            int mid = (low+high)/2;
+            int mid = low + (high-low)/2;
+
             if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
                 return mid;
             }
             else if(nums[mid] > nums[mid-1]){
-                low = mid + 1;
+                low = mid+1;
             }
             else if(nums[mid] > nums[mid+1]){
-                high = mid - 1;
+                high = mid-1;
             }
-            else low = mid + 1;
+            else{
+                low = mid+1;
+            }
         }
         return -1;
     }
