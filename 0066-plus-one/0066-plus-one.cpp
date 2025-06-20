0257736b3d7a857a372c0f93d2f08092fdbfcf49
Srishti Verma <136918651+lost-pika@users.noreply.github.com>
2025-06-20 21:33:45 +0530

    Time: 0 ms (100%), Space: 11.6 MB (34.61%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for(int i = n-1; i>=0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;  // carry to the next left digit
+        }
+
+        // If we reach here, it means all digits were 9 (e.g., 999 -> 1000)
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
