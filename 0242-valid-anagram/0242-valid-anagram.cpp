4258e6cc770e67c6134264589be7141c511f7d7d
Srishti Verma <136918651+lost-pika@users.noreply.github.com>
2025-07-08 13:52:48 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        if(s.length() != t.length()) return false;
 
-        return s == t;
+        vector<int> count(26, 0);
+
+        for(char &ch : s){
+            count[ch - 'a']++;
+        }
+
+        for(char &ch : t){
+            count[ch - 'a']--;
+        }
+
+        for(int i = 0; i<26; i++){
+            if(count[i] != 0) return false;
+        }
+
+        return true;
     }
 };
